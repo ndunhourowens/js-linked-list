ece59e106e50d28184226b7c84bc5c3a59cb46ec
Naomi Owens <naomiowens@Naomis-Mac-mini.local>
2015-09-04 12:35:28 -1000

    created module object to interact with linked list objects.  Create module obj available for linked list obj

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,38 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var train = {
+    getHead : _getHead,
+    getTail : _getTail,
+    add : _add,
+    get : _get,
+    remove : _remove,
+    insert : _insert
+  };
 
+  function _getHead() {
+
+  }
+
+  function _getTail() {
+
+  }
+
+  function _add() {
+
+  }
+
+  function _get() {
+
+  }
+
+  function _remove() {
+
+  }
+
+  function _insert() {
+
+  }
+
+  return train;
 }
