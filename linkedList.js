bb4a2b289e01ad750f42a943f3524b5860fe2826
Naomi Owens <naomiowens@Naomis-Mac-mini.local>
2015-09-05 13:37:26 -1000

    completed the remove test

--- linkedList.js
+++ linkedList.js
@@ -3,7 +3,7 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
   var train = {
     getHead : _getHead,
     getTail : _getTail,
@@ -19,8 +19,8 @@ function linkedListGenerator(){
   var length = 0;
 
   function createNode(value) {
-    return  { value : value,
-              next : null};
+    return { value : value,
+              next : null };
   }
 
   function _getHead() {
@@ -50,7 +50,7 @@ function linkedListGenerator(){
   }
 
   function _get(number) {
-    var current = train.getHead();
+    var current = head;
     //retrun false if no node is found'
     if (number >= length) {
       return false;
@@ -62,13 +62,35 @@ function linkedListGenerator(){
   }
 
   function _remove(number) {
+    var previous = _get(number - 1);
+    var currNode = _get(number);
+    var next = _get(number + 1);
 
-
-
+    // if the number is greater then the length of nodes
+    if (number >= length) {
+      return false;
+    } else if (length === 1 && number === 1) {
+      // if only have one node
+      head = null;
+      tail = null;
+    } else {
+      if ( number === 0) {// if remove the head
+        head = head.next;
+      } else if ( number === length - 1) {
+        // if remove the tail
+        tail = previous;
+        previous.next = null;
+      } else if ( number < length) {
+        // if remove the middle
+        previous.next = currNode.next;
+      }
+    }
+    length = length - 1;
   }
 
   function _insert(value,number) {
+    var newNode = _add();
 
   }
-    return train;
+  return train;
 }
